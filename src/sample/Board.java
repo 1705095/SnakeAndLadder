25073bfcd07227e276d9264205e08a657604ebb0
ArifShariar <47409421+ArifShariar@users.noreply.github.com>
2019-09-22 12:39:06 +0600

    Board

--- src/sample/Board.java
+++ src/sample/Board.java
@@ -40,7 +40,7 @@ public class Board {
         int max = 6;
         int min = 1;
         int x = (int) ((Math.random() * ((max - min) + 1)) + min);
-        System.out.println(x);
+        //System.out.println(x);
 
         if (x==1){
             diceImage.setImage(one);
