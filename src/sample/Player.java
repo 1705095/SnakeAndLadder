896ca191fb0e57dec777ed7fe88859fe4194be92
ArifShariar <47409421+ArifShariar@users.noreply.github.com>
2019-09-22 22:52:20 +0600

    Board

--- src/sample/Player.java
+++ src/sample/Player.java
@@ -1,4 +1,37 @@
 package sample;
 
 public class Player {
+        int position;
+        int color;
+        boolean stat;
+
+    public Player(int color) {
+        this.position = 0;
+        this.color = color;
+        this.stat = false;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    public void setColor(int color) {
+        this.color = color;
+    }
+
+    public boolean isStat() {
+        return stat;
+    }
+
+    public void setStat(boolean stat) {
+        this.stat = stat;
+    }
 }
