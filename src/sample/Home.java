82e20a2b66759fba6a5cca92f9382bef4c9f76db
ArifShariar <47409421+ArifShariar@users.noreply.github.com>
2019-09-23 18:36:38 +0600

    background image

--- src/sample/Home.java
+++ src/sample/Home.java
@@ -1,4 +1,48 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
 public class Home {
+
+    @FXML
+    private AnchorPane bckgnd;
+
+    @FXML
+    private ImageView backgroundImage;
+
+    @FXML
+    private CheckBox TwoPlayer;
+
+    @FXML
+    private CheckBox ThreePlayer;
+
+    @FXML
+    private CheckBox FourPlayer;
+
+    @FXML
+    private Button gameButton;
+
+    @FXML
+    private TextField play1;
+
+    @FXML
+    private TextField play2;
+
+    @FXML
+    private TextField play3;
+
+    @FXML
+    private TextField play4;
+
+    @FXML
+    void StartGame(ActionEvent event) {
+
+    }
+
 }
