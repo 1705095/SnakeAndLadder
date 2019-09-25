066f3e1e29796bb188810a0f0cfd22c0caca2d0b
ArifShariar <47409421+ArifShariar@users.noreply.github.com>
2019-09-25 19:18:51 +0600

    Added all imageView to the controller file.

--- src/sample/Board.java
+++ src/sample/Board.java
@@ -54,34 +54,23 @@ public class Board {
 
 
     @FXML
-    private ImageView b1;
+    private ImageView b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12,b13,b14,b15,b16,b17,b18,b19,b20,b21;
 
     @FXML
-    private ImageView b2;
+    private  ImageView b22,b23,b24,b25,b26,b27,b28,b29,b30,b31,b32,b33,b34,b35,b36,b37,b38,b39,b40;
 
     @FXML
-    private ImageView b3;
+    private ImageView b41,b42,b43,b44,b45,b46,b47,b48,b49,b50;
 
     @FXML
-    private ImageView b4;
+    private ImageView b51,b52,b53,b54,b55,b56,b57,b58,b59,b60,b61,b62,b63,b64,b65,b66,b67,b68,b69,b70,b71,b72,b73,b74,b75,b76,b77,b78,b79,b80;
 
     @FXML
-    private ImageView b5;
+    private ImageView b81,b82,b83,b84,b85,b86,b87,b88,b89,b90,b91,b92,b93,b94,b95,b96,b97,b98,b99,b100;
 
-    @FXML
-    private ImageView b6;
 
-    @FXML
-    private ImageView b8;
 
-    @FXML
-    private ImageView b7;
 
-    @FXML
-    private ImageView b9;
-
-    @FXML
-    private ImageView b10;
 
 
 
